0730d342c76ee7f91203078d63f78252468a1436
rosebuddy <helloladybread@gmail.com>
2016-01-14 19:40:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gemster::Application.config.secret_key_base = '3e2fcc5aaa743f6e14f139132760fa13720d670f13e62e91e50e3c61fee5ffc77b96ee97286153401541fe04952a05102d89693a23b3551ee329dfa2c1acf936'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gemster::Application.config.session_store :cookie_store, key: '_gemster_session'
