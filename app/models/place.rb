1c45e118e0b65ad8da7fe59c52076467151841c7
rosebuddy <helloladybread@gmail.com>
2016-01-28 21:05:35 +0000

    Add CarrierWave gem, create photo model, edit migration file and setup associations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+	has_many :photos
 	
 	geocoded_by :address # can also be an IP address
 	after_validation :geocode # auto-fetch coordinates
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120010928) do
+ActiveRecord::Schema.define(version: 20160128204151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20160120010928) do
 
   add_index "installs", ["email"], name: "index_installs_on_email", unique: true, using: :btree
   add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
+
+  create_table "photos", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "picture"
+  end
 
   create_table "places", force: true do |t|
     t.string   "name"
